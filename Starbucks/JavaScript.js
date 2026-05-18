21bfad5c8b429eb65bb8b95ec6a5614a9e01ca6f
Igor Fulchini <d4xvxfmzfd@privaterelay.appleid.com>
2026-05-17 23:06:46 -0300

    Update JavaScript.js

--- Starbucks/JavaScript.js
+++ Starbucks/JavaScript.js
@@ -4,11 +4,11 @@ const textoDestaque = document.getElementById('texto-destaque');
 const botoesMenu    = document.querySelectorAll('.botao-menu');
 
 function trocarBebida(botaoClicado, cor, endereco) {
-  // Atualiza cor do círculo e do texto destaque
+
   circulo.style.background  = cor;
   textoDestaque.style.color = cor;
 
-  // Troca imagem com fade suave
+
   copo.style.opacity   = '0';
   copo.style.transform = 'scale(0.95)';
   setTimeout(() => {
@@ -17,7 +17,7 @@ function trocarBebida(botaoClicado, cor, endereco) {
     copo.style.transform = 'scale(1)';
   }, 300);
 
-  // Marca botão ativo
+ 
   botoesMenu.forEach(b => b.classList.remove('ativo'));
   botaoClicado.classList.add('ativo');
 }
